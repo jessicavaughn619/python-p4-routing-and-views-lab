c7ca70d8bc25fcf4f748fd2cce464ec24f7a99b1
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-05-02 15:33:57 -0600

    partial solution

--- server/app.py
+++ server/app.py
@@ -4,5 +4,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    count = f''
+    for n in range(parameter):
+        count += f'{n}\n'
+    return count
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
